830e4d308566d94b8d70eabb75c17368ffca0f2d
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-11-25 14:26:42 -0500

    running into problem with nested routes.  working on finding a solution

--- src/App.js
+++ src/App.js
@@ -6,23 +6,27 @@ import DashLayout from './components/DashLayout';
 import Welcome from './features/auth/Welcome';
 import NotesList from './features/notes/NotesList';
 import UsersList from './features/users/UsersList';
-// import { Note } from '@material-ui/icons';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+      {/* <Route exact path="/*" element={<appbody />} /> */}
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
 
         <Route path="dash" element={<DashLayout />}>
+
           <Route index element={<Welcome />} />
+
           <Route path="notes">
             <Route index element={<NotesList />} />
           </Route>
+
           <Route path="users">
             <Route index element={<UsersList />} />
           </Route>
+
         </Route>
         
       </Route>
@@ -31,3 +35,14 @@ function App() {
 }
 
 export default App;
+
+// {
+//   "src": "logo192.png",
+//   "type": "image/png",
+//   "sizes": "192x192"
+// },
+// {
+//   "src": "logo512.png",
+//   "type": "image/png",
+//   "sizes": "512x512"
+// }
