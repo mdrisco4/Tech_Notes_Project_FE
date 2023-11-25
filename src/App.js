38fd4999f3fa25a74d0085770e151b36d1434ef1
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-11-25 13:39:06 -0500

    set up protected routes for new NOtes and Users components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Public from './components/Public';
 import Login from './features/auth/Login';
 import DashLayout from './components/DashLayout';
 import Welcome from './features/auth/Welcome';
+import NotesList from './features/notes/NotesList';
+import UsersList from './features/users/UsersList';
+// import { Note } from '@material-ui/icons';
 
 function App() {
   return (
@@ -13,9 +16,13 @@ function App() {
         <Route path="login" element={<Login />} />
 
         <Route path="dash" element={<DashLayout />}>
-
           <Route index element={<Welcome />} />
-
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
         </Route>
         
       </Route>
