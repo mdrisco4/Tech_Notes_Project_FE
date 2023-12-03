348b62a7d6cf068ca5eaf5060bd8d6cd444c8a54
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-12-02 21:14:54 -0500

    set up loading screen and data rendering on the users list

--- src/features/users/UsersList.js
+++ src/features/users/UsersList.js
@@ -1,4 +1,49 @@
+import { userGetUsersQuery } from "./usersApiSlice"
+
 const UsersList = () => {
+
+    const {
+        data: users,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = userGetUsersQuery()
+
+    let content
+
+    if (isLoading) content = <p>Loading...</p>
+
+    if (isError) {
+        content = <p className={isError ? "errmsg" : "offscreen"}>{error?.data?.message}</p>
+    }
+
+    if (isSuccess) {
+
+        const { ids } = users
+
+        const tableContent = ids?.length
+            ? ids.map(userId => <User key={userId} userId={userId} />)
+            : null
+
+        content = (
+            <table className="table table--users">
+                <head className="table__thead">
+                    <tr>
+                        <th scope="col" className="table__th
+                        user__username">Username</th>
+                        <th scope="col" className="table__th
+                        user__roles">Roles</th>
+                        <th scope="col" className="table__th
+                        user__edit">Edit</th>
+                    </tr>
+                </head>
+                <tbody>
+                    {tableContent}
+                </tbody>
+            </table>
+        )
+    }
     return (
         <h1>Users List</h1>
     )
