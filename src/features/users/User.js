9012811c6189c663c1877f2d71d4703e3a403578
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-12-02 21:31:28 -0500

    finished setting up how user is displayed

--- src/features/users/User.js
+++ src/features/users/User.js
@@ -10,9 +10,30 @@ const User = ({ userId }) => {
 
     const navigate = useNavigate()
 
-    return(
-        <div>User</div>
-    )
+    if (user) {
+        const handleEdit = () => navigate(`/dash/users/${userId}`)
+
+        const userRolesString = user.roles.toString().replaceAll(',', ', ')
+
+        const cellStatus = user.active ? '' : 'table__cell--inactive'
+
+        return (
+            <tr className="table__row user">
+                <td className={`table__cell ${cellStatus}`}>{user.username}</td>
+                <td className={`table__cell ${cellStatus}`}>{userRolesString}</td>
+                <td className={`table__cell ${cellStatus}`}>
+                    <button
+                        className="icon-button table__button"
+                        onClick={handleEdit}
+                    >
+                        <FontAwesomeIcon icon={faPenToSquare} />
+                    </button>
+                </td>
+            </tr>
+
+        )
+
+    }else return null
 }
 
 export default User
