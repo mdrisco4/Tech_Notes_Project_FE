540828310833ad331dbeef651b8dddeb1cb3000b
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-12-05 14:13:19 -0500

    EditUserForm is set up

--- src/features/users/EditUserForm.js
+++ src/features/users/EditUserForm.js
@@ -75,6 +75,16 @@ const EditUserForm = ({ user }) => {
         await deleteUser({ id: user.id })
     }
 
+    const options = Object.values(ROLES).map(role => {
+        return (
+            <option
+                key={role}
+                value={role}
+
+            > {role}</option >
+        )
+    })
+
     let canSave
     if (password) {
         canSave = [roles.length, validUsername, validPassword].every(Boolean) && !isLoading
@@ -89,8 +99,87 @@ const EditUserForm = ({ user }) => {
 
     const errContent = (error?.data?.message || delError?.data?.message) ?? ''
 
-    return (
-        <div>EditUserForm</div>
+    const content = (
+        <>
+            <p className={errClass}>{errContent}</p>
+
+            <form className="form" onSubmit={e => e.preventDefault()}>
+                <div className="form__title-row">
+                    <h2>Edit User</h2>
+                    <div className="form__action-buttons">
+                        <button
+                            className="icon-button"
+                            title="Save"
+                            onClick={onSaveUserClicked}
+                            disabled={!canSave}
+                        >
+                            <FontAwesomeIcon icon={faSave} />
+                        </button>
+                        <button
+                            className="icon-button"
+                            title="Delete"
+                            onClick={onDeleteUserClicked}
+                        >
+                            <FontAwesomeIcon icon={faTrashCan} />
+                        </button>
+                    </div>
+                </div>
+                <label className="form__label" htmlFor="username">
+                    Username: <span className="nowrap">[3-20 letters]</span>
+                </label>
+                <input
+                    className={`form__input ${validUserClass}`}
+                    id="username"
+                    name="username"
+                    type="text"
+                    autoComplete="off"
+                    value={username}
+                    onChange={onUserNameChanged}
+                />
+
+                <label className="form__label" htmlFor="password">
+                    Password: <span className="nowrap">[empty = no change]</span>  <span className="nowrap">[4-12 chars incl. !@#$%]</span>
+                </label>
+                <input
+                    className={`form__input ${validPwdClass}`}
+                    id="password"
+                    name="password"
+                    type="password"
+                    value="password"
+                    onChange={onPasswordChanged}
+                />
+
+                <label className="form__label form__checkbox-container" htmlFor="user-active">
+                    ACTIVE:
+                    <input
+                        className="form__checkbox"
+                        id="user-active"
+                        name="user-active"
+                        type="checkbox"
+                        checked={active}
+                        onChange={onActiveChanged}
+                    />
+                </label>
+
+                <label className="form__label" htmlFor="roles">
+                    ASSIGNED ROLES:
+                </label>
+                    <select
+                        id="roles"
+                        name="roles"
+                        className={`form__select ${validRolesClass}`}
+                        multiple={true}
+                        size="3"
+                        value={roles}
+                        onChange={onRolesChanged}
+                    >
+                        {options}
+                    </select>
+                
+            </form>
+        </>
     )
+
+    return content
 }
 export default EditUserForm
