793633d8d7beb5a6427c75a73e2db077c682b906
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-12-05 23:30:11 -0500

    setting up Prefetch so forms do not time out without subscriptions

---
+++ src/features/auth/Prefetch.js
@@ -0,0 +1,12 @@
+import { store }  from '../../app/store'
+import { notesApiSlice } from '../notes/notesApiSlice'
+import { usersApiSlice } from '../users/usersApiSlice'
+import { useEffect } from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Prefetch = () => {
+    return (
+        <div>Prefetch</div>
+    )
+}
+export default Prefetch
