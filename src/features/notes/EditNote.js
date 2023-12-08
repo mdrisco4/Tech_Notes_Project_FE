f71f7708c9eff36a8a17a471263f17100aba9ef0
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-12-08 16:17:15 -0500

    continuing to work on EditNoteForm

--- src/features/notes/EditNote.js
+++ src/features/notes/EditNote.js
@@ -5,6 +5,14 @@ import { selectAllUsers } from "../users/usersApiSlice"
 import EditNoteForm from "./EditNoteForm"
 
 const EditNote = () => {
+
+    const { id } = useParams()
+
+    const note = useSelector(state => selectNoteById(state, id))
+    const users = useSelector(selectAllUsers)
+
+    const content = note && users ? <EditNoteForm note={note} users={users} /> : <p>...Loading</p>
+
     return (
         <div>EditNote</div>
     )
--- src/features/notes/EditNoteForm.js
+++ src/features/notes/EditNoteForm.js
@@ -1,6 +1,41 @@
+import { useState, useEffect } from "react"
+import { useUpdateNoteMutation, useDeleteNoteMutation } from "./notesApiSlice"
+import { useNavigate } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons"
+
 const EditNoteForm = () => {
-    return (
-        <div>EditNoteForm</div>
-    )
+
+    const [updateNote, {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }] = useUpdateNoteMutation
+
+    const [deleteNote, {
+        isSuccess: isDelSuccess,
+        isError: isDelError,
+        error: delError
+    }] = useDeleteNoteMutation
+
+    const navigate = useNavigate()
+
+    const [title, setTitle] = useState(note.title)
+    const [text, setText] = useState(note.text)
+    const [completed, setCompleted] = useState(note.completed)
+    const [userId, setUserId] = useState(note.user)
+
+    useEffect(() => {
+
+        if (isSuccess || isDelSuccess) {
+            setTitle('')
+            setText('')
+            setUserId('')
+            navigate('/dash/notes')
+        }
+    }, [isSuccess, isDelSuccess, navigate])
+
+    return content
 }
 export default EditNoteForm
