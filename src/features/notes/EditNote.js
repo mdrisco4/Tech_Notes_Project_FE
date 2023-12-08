1b8bc469798b09eba69ed75d9f0d3c4cd424ca4e
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-12-08 16:04:10 -0500

    created EditNotForm to be built later and started working on EditNote

--- src/features/notes/EditNote.js
+++ src/features/notes/EditNote.js
@@ -1,3 +1,9 @@
+import { useParams } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { selectNoteById } from "./notesApiSlice"
+import { selectAllUsers } from "../users/usersApiSlice"
+import EditNoteForm from "./EditNoteForm"
+
 const EditNote = () => {
     return (
         <div>EditNote</div>
