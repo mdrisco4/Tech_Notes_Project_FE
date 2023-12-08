f63f2263d87f6da2337407ad1e1dfb92a44ce0cb
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-12-08 16:28:07 -0500

    set up functionality part of EditNoteForm, need to build the form UI

--- src/features/notes/EditNoteForm.js
+++ src/features/notes/EditNoteForm.js
@@ -36,6 +36,49 @@ const EditNoteForm = () => {
         }
     }, [isSuccess, isDelSuccess, navigate])
 
+    const onTitleChanged = e => setTitle(e.target.value)
+    const onTextChanged = e => setText(e.target.value)
+    const onCompletedChanged = e => setCompleted(e.target.value)
+    const onUserIdChanged = e => setUserId(e.target.value)
+
+    const canSave = [title, text,userId].every(Boolean) && !isLoading
+
+    const on onSaveNoteClicked = async (e) => {
+        if (canSave) {
+            await updateNote({ id: note.id, user: userId, title, text, completed })
+        }
+    }
+
+    const onDeleteUserClicked = async () => {
+        await deleteNote({ id: note.id })
+    }
+
+    const created = new Date(note.createdAt).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })
+    const updated = new Date(note.createdAt).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })
+
+    const options = users.map(user => {
+        return (
+            <option
+            key={user.id}
+            value={user.id}
+
+            >
+                {user.username}
+            </option>
+        )
+    })
+
+    const errClass = (isError || isDelError) ? "errmsg" : "offscreen"
+    const validTitleClass = !title ? "form__input--incomplete" : ''
+    const validTextClass = !title ? "form__input--incomplete" : ''
+
+    const errContent = (error?.data?.message || delError?.data?.message) ?? ''
+
+    const content = (
+        <>
+        </>
+    )
+
     return content
 }
 export default EditNoteForm
