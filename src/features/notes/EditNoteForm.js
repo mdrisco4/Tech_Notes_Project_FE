b3162dc2e4b9c9d146d3205960abe7e181e6bd4f
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-12-09 15:20:50 -0500

    built out EditNoteForm structure

--- src/features/notes/EditNoteForm.js
+++ src/features/notes/EditNoteForm.js
@@ -49,7 +49,7 @@ const EditNoteForm = () => {
         }
     }
 
-    const onDeleteUserClicked = async () => {
+    const onDeleteNoteClicked = async () => {
         await deleteNote({ id: note.id })
     }
 
@@ -76,6 +76,85 @@ const EditNoteForm = () => {
 
     const content = (
         <>
+            <p className={errClass}>{errContent}</p>
+
+            <form className="form" onSubmit={e => e.preventDefault()}>
+                <div className="form__title-row">
+                    <h2>Edit Note #{note.ticket}</h2>
+                    <div className="form__action-buttons">
+                        <button
+                            className="icon-button"
+                            title="Save"
+                            onClick={onSaveUserClicked}
+                            disabled={!canSave}
+                        >
+                            <FontAwesomeIcon icon={faSave} />
+                        </button>
+                        <button
+                            className="icon-button"
+                            title="Delete"
+                            onClick={onDeleteNoteClicked}
+                        >
+                            <FontAwesomeIcon icon={faTrashCan} />
+                        </button>
+                    </div>
+                </div>
+                <label className="form__label" htmlFor="note-title">
+                    Title:
+                </label>
+                <input
+                    className={`form__input ${validTitleClass}`}
+                    id="note-title"
+                    name="title"
+                    type="text"
+                    autoComplete="off"
+                    value={title}
+                    onChange={onTitleChanged}
+                />
+
+                <label className="form__label" htmlFor="note-text">
+                    Text:
+                </label>
+                <textarea
+                    className={`form__input form__input--text ${validTextClass}`}
+                    id="note-text"
+                    name="text"
+                    value={text}
+                    onChange={onTextChanged}
+                />
+                <div className="form__row">
+                    <div className="form__divider">
+                        <label className="form__label form__checkbox-container" htmlFor="note-completed">
+                            WORK COMPLETE:
+                            <input
+                                className="form__checkbox"
+                                id="note-completed"
+                                name="completed"
+                                type="checkbox"
+                                checked={completed}
+                                onChange={onCompletedChanged}
+                            />
+                        </label>
+
+                        <label className="form__label form__checkbox-container" htmlFor="note-username">
+                            ASSIGNED TO:
+                        </label>
+                        <select
+                            id="note-username"
+                            name="username"
+                            className="form__select"
+                            value={userid}
+                            onChange={onUserIdChanged}
+                        >
+                            {options}
+                        </select>
+                    </div>
+                    <div className="form__divider">
+                        <p className="form__created">Created:<br />{created}</p>
+                        <p className="form__updated">Updated:<br />{updated}</p>
+                    </div>
+                </div>
+            </form>
         </>
     )
 
